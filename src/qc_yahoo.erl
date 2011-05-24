cf11a7d517dbf7b6be5e572562b8ba66a92330fb
Erik Axling <axling@gmail.com>
2011-05-24 22:55:39 +0200

    Added options to get, can now fetch ask,bid,high,low,open,close for any quote

--- src/qc_yahoo.erl
+++ src/qc_yahoo.erl
@@ -10,8 +10,19 @@
 
 -define(BASE, "http://finance.yahoo.com/d/quotes.csv").
 
--spec get(Quotes :: [atom()], Types :: [atom()]) ->
+
+-spec get(Quotes :: [atom()], Types :: [ask | bid | high| low |
+					open | last_close | string()]) ->
 		 list() | {error, Reason :: any()}.
+%% @doc Will get the values specified in <code>Types</code> and return them
+%% in a property list. 
+%% ask = asking price for the stock
+%% bid = biddin price for the stock
+%% high = highest value for the day
+%% low = lowest value for the day
+%% open = value the stock opened on
+%% last_close = Previous day's closing value
+%% The return value will be returned as such [{Quote, [{Type, Value}]}].
 get(Quotes, Types) when is_list(Quotes), is_list(Types) ->
     YahooQuotes = string:join([atom_to_list(Quote) || Quote <- Quotes], "+"),
     YahooTypes = lists:concat([convert_type(Type) || Type <- Types]),
@@ -57,11 +68,31 @@ parse_request_body([QuotePart | Body], [Quote | Quotes], Types, AccResult) ->
     
 parse_result(ask, Result) ->
     {ask, list_to_number(Result)};
+parse_result(bid, Result) ->
+    {bid, list_to_number(Result)};
+parse_result(high, Result) ->
+    {high, list_to_number(Result)};
+parse_result(low, Result) ->
+    {low, list_to_number(Result)};
+parse_result(open, Result) ->
+    {open, list_to_number(Result)};
+parse_result(last_close, Result) ->
+    {last_close, list_to_number(Result)};
 parse_result(Else, Result) ->
     {Else, Result}.
 
 convert_type(ask) ->
     "a";
+convert_type(bid) ->
+    "b";
+convert_type(high) ->
+    "h";
+convert_type(low) ->
+    "g";
+convert_type(open) ->
+    "o";
+convert_type(last_close) ->
+    "p";
 convert_type(Else) ->
     Else.
 
--- test/qc_yahoo_test.erl
+++ test/qc_yahoo_test.erl
@@ -24,5 +24,40 @@ get_test() ->
 		end),
     [{'eric-b.st', [{ask, 90.8}]}] = qc_yahoo:get(['eric-b.st'], [ask]),
     true = meck:validate(httpc),
+    meck:unload(httpc),
+    %% Test with ask and bid price 
+    meck:new(httpc),
+    meck:expect(httpc, request, 
+		fun(_Url) -> 
+			{ok, {bla, lsda,
+			      "90.80,10.4\r\n"}} 
+		end),
+    [{'eric-b.st', [{ask, 90.8},{"b", "10.4"}]}] = 
+	qc_yahoo:get(['eric-b.st'], [ask, "b"]),
+    true = meck:validate(httpc),
+    meck:unload(httpc),
+    %% Tets with several quotes
+    meck:new(httpc),
+    meck:expect(httpc, request, 
+		fun(_Url) -> 
+			{ok, {bla, lsda,
+			      "90.80,10.4\r\n10,11.3\r\n"}} 
+		end),
+    [{'eric-b.st', [{ask, 90.8},{"b", "10.4"}]},
+     {'abb.st', [{ask, 10}, {"b", "11.3"}]}] = 
+	qc_yahoo:get(['eric-b.st', 'abb.st'], [ask, "b"]),
+    true = meck:validate(httpc),
+    meck:unload(httpc),
+    %% Test with all allowed atom values
+    meck:new(httpc),
+    meck:expect(httpc, request, 
+		fun(_Url) -> 
+			{ok, {bla, lsda,
+			      "90.80,10.4,10,11.3\r\n"}} 
+		end),
+    [{'eric-b.st', [{ask, 90.8},{bid, 10.4}, {high, 10}, {low, 11.3}]}] = 
+	qc_yahoo:get(['eric-b.st'], [ask, bid, high, low]),
+    true = meck:validate(httpc),
     meck:unload(httpc).
+
     
